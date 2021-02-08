2fb08eb9f24dd8bbf507bcaf0aab47b5f49c46b8
B-Iglesia <bradiglesia@gmail.com>
2021-02-07 21:01:46 -0800

    Added UI and powerups controller. Added basic cooldown UI

---
+++ Sun Son/Assets/Scripts/PowerupController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerupController : MonoBehaviour
+{
+    [SerializeField] Image _dashCooldownImage;
+    [SerializeField] Player _player;
+    private float _dashCooldown;
+
+    void Start() {
+        _dashCooldownImage.fillAmount = 0;
+    }
+    public void activateDashCooldown(bool isCooldown) {
+        if (isCooldown) {
+             timeCooldown(_dashCooldown, _dashCooldownImage);
+        }
+    }
+
+    public void timeCooldown(float cooldown, Image image) {
+        image.fillAmount += 1 / cooldown * Time.deltaTime;
+        if (image.fillAmount >= 1) {
+            image.fillAmount = 0;
+            _player.setIsDashCooldown(false);
+        }
+    }
+
+    public void setDashCooldown(float dashCooldown) {
+        _dashCooldown = dashCooldown;
+    }
+}
---
+++ Sun Son/Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Player _player;
+    [SerializeField] PowerupController _powerups;
+    [SerializeField] LightBar _lightBar;
+    private int _currentLightPoints;
+    private int _maxLightPoints;
+    void Start()
+    {
+        _maxLightPoints = _player.getMaxLightPoints();
+        _currentLightPoints = _maxLightPoints;
+        _lightBar.SetMaxLightPoints(_currentLightPoints);
+        _lightBar.SetLightPoints(_currentLightPoints);
+        _powerups.setDashCooldown(_player.getDashDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _lightBar.SetLightPoints(_player.getCurrentLightPoints());
+        _powerups.activateDashCooldown(_player.getIsDashCooldown());
+    }
+}
