70c6fbd699127446e68e21dcfaaa772eac7d255e
oerlich <oren.e@live.com>
2021-02-10 02:21:49 -0800

    New and Improved Input System (w/ Controller Support\!)

---
+++ Sun Son/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,307 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""CharacterControls"",
+            ""id"": ""107b4469-3777-43c1-a908-83e28dbd285f"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""aa921d87-7dae-4f58-b9d1-4e6729add4be"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""613feff7-8f7a-4c81-8f64-e309594a396d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)""
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""b37f4801-43e5-454a-b6cb-7fd8bd38c814"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)""
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""90310259-5df6-401a-a3f6-5d947d39a645"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""b69310f0-4b70-4874-b886-6dc5845cbcc7"",
+                    ""path"": ""<Keyboard>/leftAlt"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""69f77c9c-e07c-4f30-860a-624606f1e1e7"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3a9d7dab-84a2-4a5a-b0e8-9cb08a49235f"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d58703e6-e052-4b16-af59-2fac60bba17e"",
+                    ""path"": ""<Gamepad>/leftShoulder"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""0f399fca-296b-49e2-a22b-b30c15bf30da"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ef547b9a-d62a-4fe5-8d75-4b3382f1d2cc"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""bc36a7bb-36b7-4d0c-a864-730863529b9e"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""d48f1d3e-2ed3-403f-9c0d-09bfb48d4e7a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""f716f71d-0849-4a25-b138-b38c3a78fe5b"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a7598887-ffa1-40e2-b507-64609cbf6dd2"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone(min=0.95)"",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6011239f-f8e4-4322-b509-ff1914f9720f"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6f77358e-2e07-4596-bc64-80b3baea30cb"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // CharacterControls
+        m_CharacterControls = asset.FindActionMap("CharacterControls", throwIfNotFound: true);
+        m_CharacterControls_Movement = m_CharacterControls.FindAction("Movement", throwIfNotFound: true);
+        m_CharacterControls_Dash = m_CharacterControls.FindAction("Dash", throwIfNotFound: true);
+        m_CharacterControls_Run = m_CharacterControls.FindAction("Run", throwIfNotFound: true);
+        m_CharacterControls_Jump = m_CharacterControls.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // CharacterControls
+    private readonly InputActionMap m_CharacterControls;
+    private ICharacterControlsActions m_CharacterControlsActionsCallbackInterface;
+    private readonly InputAction m_CharacterControls_Movement;
+    private readonly InputAction m_CharacterControls_Dash;
+    private readonly InputAction m_CharacterControls_Run;
+    private readonly InputAction m_CharacterControls_Jump;
+    public struct CharacterControlsActions
+    {
+        private @PlayerControls m_Wrapper;
+        public CharacterControlsActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_CharacterControls_Movement;
+        public InputAction @Dash => m_Wrapper.m_CharacterControls_Dash;
+        public InputAction @Run => m_Wrapper.m_CharacterControls_Run;
+        public InputAction @Jump => m_Wrapper.m_CharacterControls_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_CharacterControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterControlsActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterControlsActions instance)
+        {
+            if (m_Wrapper.m_CharacterControlsActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMovement;
+                @Dash.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnDash;
+                @Run.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Jump.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_CharacterControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public CharacterControlsActions @CharacterControls => new CharacterControlsActions(this);
+    public interface ICharacterControlsActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Sun Son/Assets/Scripts/PlayerV2.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerV2 : MonoBehaviour
+{
+
+    // Fields for movement characteristics
+    [SerializeField] float _jumpHeight = 1;
+    [SerializeField] float _walkSpeed = 2;
+    [SerializeField] float _sprintSpeed = 4;
+    private float _gravityValue = Physics.gravity.y;
+    private Vector3 _moveDirection = Vector3.zero;
+    private CharacterController _controller;
+    private Vector3 _playerVelocity;
+    private float _playerSpeed;
+    private bool _grounded;
+
+    // Fields for dash ability
+    [SerializeField] float _maxDashTime = 1.0f;
+    [SerializeField] float _dashSpeed = 10.0f;
+    [SerializeField] float _dashStoppingSpeed = 0.1f;
+    [SerializeField] float _dashDelay = 2f;
+    [SerializeField] int _dashCost = 20;
+    private float _currentDashTime;
+    private bool _isDashCooldown;
+    private float _nextDashAvailable;
+
+    // Fields for Player Resources
+    [SerializeField] int _maxLightPoints;
+    private int _currentLightPoints;
+
+    // Fields for player input
+    private PlayerControls _input;
+    private float _currentMovement;
+    private bool _movementPressed;
+    private bool _runPressed;
+    private bool _dashPressed;
+    private bool _jumpPressed;
+
+    // Fields for Animations
+    private Animator _anim;
+    private int _isWalkingHash;
+    private int _isRunningHash;
+    private int _isJumpingHash;
+    private int _isFallingHash;
+    private int _isDashingHash;
+    private int _isShieldingHash;
+    private int _isFiringRangedHash;
+    private int _isMeleeingHash;
+
+    // Fields for FX
+    [SerializeField] TrailRenderer _trailRenderer;
+    [SerializeField] GameObject _pointLight;
+
+    // Fields for UI Elements
+    [SerializeField] LightBar _lightBar;
+
+    // Main Camera
+    private Camera _mainCamera;
+
+
+
+    private void Awake()
+    {
+        _input = new PlayerControls();
+
+        _input.CharacterControls.Movement.performed += ctx =>
+        {   
+            Vector2 temp = ctx.ReadValue<Vector2>();
+            _currentMovement = temp.x;
+            _movementPressed = _currentMovement != 0;
+        };
+
+        _input.CharacterControls.Dash.performed += ctx => _dashPressed = ctx.ReadValueAsButton();
+
+        _input.CharacterControls.Run.performed += ctx => _runPressed = ctx.ReadValueAsButton();
+
+        _input.CharacterControls.Jump.performed += ctx => _jumpPressed = ctx.ReadValueAsButton();
+    }
+
+    private void OnEnable()
+    {
+        _input.CharacterControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.CharacterControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
+        _mainCamera = Camera.main;
+
+        _isWalkingHash = Animator.StringToHash("isWalking");
+        _isRunningHash = Animator.StringToHash("isRunning");
+        _isJumpingHash = Animator.StringToHash("isJumping");
+        _isFallingHash = Animator.StringToHash("isFalling");
+        _isDashingHash = Animator.StringToHash("isDashing");
+        _isShieldingHash = Animator.StringToHash("isShielding");
+        _isFiringRangedHash = Animator.StringToHash("isFiringRanged");
+        _isMeleeingHash = Animator.StringToHash("isMeleeing");
+
+        _currentDashTime = _maxDashTime;
+
+        _currentLightPoints = _maxLightPoints;
+        _lightBar.SetMaxLightPoints(_maxLightPoints);
+        _lightBar.SetLightPoints(_maxLightPoints);
+        _pointLight.GetComponent<LightPower>().SetMaxLightPoints(_currentLightPoints);
+        _pointLight.GetComponent<LightPower>().SetLightPoints(_maxLightPoints);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+
+        _grounded = _controller.isGrounded;
+        if (_grounded && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = 0f;
+            _anim.SetBool(_isFallingHash, false);
+        }
+        else if(!_grounded && _playerVelocity.y < 0)
+        {
+            _anim.SetBool(_isFallingHash, true);
+        }
+
+        handleDirection();
+        handleGroundMovement();
+        handleJumping();
+        handleDashing();
+
+        _playerVelocity.y += _gravityValue * Time.deltaTime;
+        _controller.Move(_playerVelocity * Time.deltaTime);
+    }
+
+    void handleDirection()
+    {
+        if (_currentMovement > 0)
+            this.transform.forward = _mainCamera.transform.right;
+        else if (_currentMovement < 0)
+            this.transform.forward = -_mainCamera.transform.right;
+    }
+
+    void handleGroundMovement()
+    {
+        _moveDirection = _mainCamera.transform.right * _currentMovement;
+        bool isRunning = _anim.GetBool(_isRunningHash);
+        bool isWalking = _anim.GetBool(_isWalkingHash);
+
+        if (!_movementPressed)
+            _playerSpeed = 0;
+
+        if(_movementPressed && !isWalking)
+        {
+            _anim.SetBool(_isWalkingHash, true);
+        }
+
+        if(!_movementPressed && isWalking)
+        {
+            _anim.SetBool(_isWalkingHash, false);
+        }
+
+        if((_movementPressed && _runPressed) && !isRunning)
+        {
+            _anim.SetBool(_isRunningHash, true);
+        }
+
+        if (!(_movementPressed && _runPressed) && isRunning)
+        {
+            _anim.SetBool(_isRunningHash, false);
+        }
+
+        if (isRunning)
+            _playerSpeed = Mathf.Lerp(_playerSpeed, _sprintSpeed, 0.2f);
+        else if (isWalking)
+            _playerSpeed = Mathf.Lerp(_playerSpeed, _walkSpeed, 0.2f);
+
+        if (isRunning || isWalking)
+            _controller.Move(_moveDirection * Time.deltaTime * _playerSpeed);
+    }
+
+    void handleJumping()
+    {
+        if(_jumpPressed && _grounded)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.5f * _gravityValue);
+            _anim.SetTrigger(_isJumpingHash);
+        }
+    }
+
+    void handleDashing()
+    {
+        if (Time.time >= _nextDashAvailable && _currentLightPoints >= _dashCost)
+        {
+            if (_dashPressed && _movementPressed)
+            {
+                _anim.SetTrigger(_isDashingHash);
+                _isDashCooldown = true;
+                _currentDashTime = 0.0f;
+                _currentLightPoints -= _dashCost;
+                _trailRenderer.enabled = true;
+                _lightBar.SetLightPoints(_currentLightPoints);
+                _pointLight.GetComponent<LightPower>().SetLightPoints(_currentLightPoints);
+                _nextDashAvailable = Time.time + _dashDelay;
+            }
+        }
+
+        if (_currentDashTime < _maxDashTime)
+            _currentDashTime += _dashStoppingSpeed;
+        else
+        {
+            _moveDirection = Vector3.zero;
+            _trailRenderer.enabled = false;
+        }
+
+        _controller.Move(_moveDirection * Time.deltaTime * _dashSpeed);
+    }
+
+    public int getMaxLightPoints() { return _maxLightPoints; }
+    public int getCurrentLightPoints() { return _currentLightPoints; }
+    public float getDashDelay() { return _dashDelay; }
+    public bool getIsDashCooldown() { return _isDashCooldown; }
+    public void setIsDashCooldown(bool cooldown) { _isDashCooldown = cooldown; }
+}
