06309fc17f8be2b706ab288fcf578f552b57d208
Austin Shin <amshin@calpoly.edu>
2021-02-11 02:43:29 -0800

    Add second obstacle
    No falling ground yet

--- Sun Son/Assets/Scripts/TempCameraMvmt.cs
+++ Sun Son/Assets/Scripts/TempCameraMvmt.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TempCameraMvmt : MonoBehaviour {
 
    public GameObject player;
+   private static float camBaseHeight = 2.0f;
 
    void Update() {
       Vector3 camPos = gameObject.transform.position;
@@ -13,6 +14,14 @@ public class TempCameraMvmt : MonoBehaviour {
       }
       else if (player.transform.position.x <= camPos.x - 7.5) {
          gameObject.transform.position = camPos - new Vector3(14f, 0, 0);
+      }
+
+      camPos = gameObject.transform.position;
+      if (player.transform.position.y >= -1.0f) {
+         gameObject.transform.position = new Vector3(camPos.x, camBaseHeight, camPos.z);
+      }
+      else if (player.transform.position.y < -1.0f) {
+         gameObject.transform.position = new Vector3(camPos.x, -camBaseHeight, camPos.z);
       }
    }
 }
