0635da76193acf1ed58b837445a3e7e1ea83e10e
Austin Shin <amshin@calpoly.edu>
2021-02-11 02:10:40 -0800

    Add basic horizontal camera movement
    For testing

---
+++ Sun Son/Assets/Scripts/TempCameraMvmt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCameraMvmt : MonoBehaviour {
+
+   public GameObject player;
+
+   void Update() {
+      Vector3 camPos = gameObject.transform.position;
+      if (player.transform.position.x >= camPos.x + 7.5) {
+         gameObject.transform.position = camPos + new Vector3(14f, 0, 0);
+      }
+      else if (player.transform.position.x <= camPos.x - 7.5) {
+         gameObject.transform.position = camPos - new Vector3(14f, 0, 0);
+      }
+   }
+}
