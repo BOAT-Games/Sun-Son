c1991d81b0f129bc318b53c63adfd485c70d8874
Taylor Hayase <thayase@calpoly.edu>
2021-02-16 15:34:42 -0800

    basic npc

---
+++ Sun Son/Assets/Scripts/Billboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    //code from Haungs
+    public Camera cam;
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.transform.forward);
+        //transform.LookAt(cam.transform);  // This does not look good for this example
+        //transform.Rotate(0, 180, 0);
+
+    }
+}
---
+++ Sun Son/Assets/Scripts/TalkController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkController : MonoBehaviour
+{
+    [SerializeField] TalkText _talkText;
+    [SerializeField] PlayerV2 _playerV2;
+
+    private PlayerControls _input;
+    private bool _interactPressed;
+
+    private bool convoStarted = false;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _input = new PlayerControls();
+        _input.CharacterControls.Interact.performed += ctx => _interactPressed = ctx.ReadValueAsButton();
+        _input.CharacterControls.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("interact: " + _interactPressed);
+        if ((_interactPressed || convoStarted) && _playerV2.getCanTalk())
+        {
+            _talkText.updateText("Hey Sun");
+            convoStarted = true;
+        }
+        else if (_playerV2.getCanTalk() && !convoStarted)
+        {
+            _talkText.updateText("Press E to talk.");
+        }
+        else
+        {
+            _talkText.updateText("");
+            convoStarted = false;
+        }
+
+    }
+}
