f3804417fba2b701df8ad41989c66b561c98727b
Taylor Hayase <thayase@calpoly.edu>
2021-02-16 15:52:19 -0800

    variable length conversation

--- Sun Son/Assets/Scripts/TalkController.cs
+++ Sun Son/Assets/Scripts/TalkController.cs
@@ -11,28 +11,55 @@ public class TalkController : MonoBehaviour
     private bool _interactPressed;
 
     private bool convoStarted = false;
-    // Start is called before the first frame update
+    private int convCount = 0;
+    private bool _wasPressed = false;
+
+    public string[] convo;
+    
     private void Awake()
     {
         _input = new PlayerControls();
         _input.CharacterControls.Interact.performed += ctx => _interactPressed = ctx.ReadValueAsButton();
         _input.CharacterControls.Enable();
     }
+    private void Start()
+    {
+        convo = new string[] {"Hey Sun.",
+            "How\'s the mission coming along?",
+            "Try not to die out there!" };
 
+    }
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("interact: " + _interactPressed);
-        if ((_interactPressed || convoStarted) && _playerV2.getCanTalk())
+        if (_interactPressed && !_wasPressed && convoStarted && _playerV2.getCanTalk())
         {
-            _talkText.updateText("Hey Sun");
-            convoStarted = true;
+            if (convCount == convo.Length)
+            {
+                _talkText.updateText("");
+            }
+            else
+            {
+                _talkText.updateText(convo[convCount]);
+                convoStarted = true;
+
+                if (convCount < convo.Length)
+                {
+                    convCount++;
+                }
+            }
+            _wasPressed = true;
         }
-        else if (_playerV2.getCanTalk() && !convoStarted)
+        if (!_interactPressed && _wasPressed)
+        {
+            _wasPressed = false;
+        }
+        else if (_playerV2.getCanTalk() && !convoStarted && convCount != convo.Length)
         {
             _talkText.updateText("Press E to talk.");
+            convoStarted = true;
         }
-        else
+        else if(!_playerV2.getCanTalk())
         {
             _talkText.updateText("");
             convoStarted = false;
