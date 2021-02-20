95c6f567bb3bf79b7b9fb4db5135ddbacdb27f5c
B-Iglesia <bradiglesia@gmail.com>
2021-02-20 14:43:55 -0800

    Manually checked that volume updates the volume

--- Sun Son/Assets/OptionsMenu.cs
+++ Sun Son/Assets/OptionsMenu.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] AudioMixer mixer;
     public void SetVolume(float volume) {
+        mixer.SetFloat("Volume", volume);
+    }
 
+    public void SetQuality(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen) {
+        Screen.fullScreen = isFullScreen;
     }
 }
