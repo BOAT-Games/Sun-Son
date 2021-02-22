b63f4a8848af5f64d7bacdf376fefdb8b128f853
Austin Shin <amshin@calpoly.edu>
2021-02-21 21:03:03 -0800

    Add destructable ground placeholders

---
+++ Sun Son/Assets/Scripts/DestructableGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableGround : MonoBehaviour
+{
+   [SerializeField] GameObject _destroyedGround;
+   [SerializeField] List<GameObject> _groundPieces;
+
+   // Start is called before the first frame update
+   void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
