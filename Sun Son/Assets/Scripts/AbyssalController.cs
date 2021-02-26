7a4ca3ac3f07b81d71b169fab1769eea2070a361
Taylor Hayase <thayase@calpoly.edu>
2021-02-26 13:48:50 -0800

    adding effects to teleportation

--- Sun Son/Assets/Scripts/AbyssalController.cs
+++ Sun Son/Assets/Scripts/AbyssalController.cs
@@ -12,12 +12,16 @@ public class AbyssalController : MonoBehaviour
     [SerializeField] bool _atPlayer = false;
     [SerializeField] bool _attacked = false;
     [SerializeField] bool _inRange = false;
+    [SerializeField] bool _portalSet = false;
 
     private Animator _anim;
     private int _isAttackingHash;
 
     private GameObject _player;
     private Vector3 startPosition;
+    [SerializeField] GameObject portal;
+    [SerializeField] GameObject flash;
+    private GameObject portalClone;
 
     public Material red;
     public Material originalMaterial;
@@ -42,16 +46,30 @@ public class AbyssalController : MonoBehaviour
 
         if (_timer <= 0)
         {
-            if (!_atPlayer && !_attacked)
-            {
-                //teleport to player
-                Vector3 targetPos = new Vector3(_player.transform.position.x - 1.2f,
+            Vector3 targetPos = new Vector3(_player.transform.position.x - 1.2f,
                                                 _player.transform.position.y,
                                                 _player.transform.position.z);
 
+            if (!_portalSet && !_atPlayer && !_attacked)
+            {
+                //set portal next to player
+                Vector3 portalPos = new Vector3(targetPos.x, targetPos.y + 0.1f, targetPos.z);
+                portalClone = Instantiate(portal, portalPos,
+                                Quaternion.LookRotation(Vector3.up, Vector3.up));
+                _portalSet = true;
+                _timer = 2;
+            }
+            else if (_portalSet && !_atPlayer && !_attacked)
+            {
+                //teleport to player
+                Vector3 flashPos = new Vector3(targetPos.x, targetPos.y + 1.3f, targetPos.z - 1);
+                Instantiate(flash, flashPos, Quaternion.LookRotation(Vector3.up, Vector3.up));
+
+                Destroy(portalClone);
                 transform.position = targetPos;
 
                 _atPlayer = true;
+                _portalSet = false;
                 _timer = 1;
             }
             else if (_atPlayer && !_attacked)
@@ -60,13 +78,28 @@ public class AbyssalController : MonoBehaviour
                 _attacked = true;
                 _timer = 2;
             }
-            else if (_atPlayer && _attacked)
+            else if (!_portalSet && _atPlayer && _attacked)
+            {
+                //set portal to be at original position
+                Vector3 portalPos = new Vector3(startPosition.x, startPosition.y + 0.1f, startPosition.z);
+                portalClone = Instantiate(portal, portalPos,
+                                Quaternion.LookRotation(Vector3.up, Vector3.up));
+                _portalSet = true;
+                _timer = 2;
+            }
+            else if (_portalSet && _atPlayer && _attacked)
             {
                 _anim.SetBool(_isAttackingHash, false);
+
+                Vector3 flashPos = new Vector3(startPosition.x, startPosition.y + 1.3f, startPosition.z - 1);
+                Instantiate(flash, flashPos, Quaternion.LookRotation(Vector3.up, Vector3.up));
+
                 //teleport back to original position
+                Destroy(portalClone);
                 transform.position = startPosition;
                 _atPlayer = false;
                 _attacked = false;
+                _portalSet = false;
                 _timer = 5;
             }
         }
