31f05baca2bc8fd83ad78d02fcbcd9ea0d5f00a0
B-Iglesia <bradiglesia@gmail.com>
2021-02-27 15:17:00 -0800

    Added basic interactability to the altar

---
+++ Sun Son/Assets/Scripts/Altar.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : MonoBehaviour
+{
+    public void placeHolder() {
+        Debug.Log("Testing Interacte");
+    }
+}
---
+++ Sun Son/Assets/Scripts/Interactable.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    private PlayerControls _input;
+    private bool _interactPressed;
+    private bool _isInRange;
+    [SerializeField] KeyCode _interactKey;
+    [SerializeField] UnityEvent _interactAction;
+
+    public void Awake() {
+        _input = new PlayerControls();
+
+        _input.CharacterControls.Interact.performed += ctx => _interactPressed = ctx.ReadValueAsButton();
+ 
+    }
+
+    private void OnEnable()
+    {
+        _input.CharacterControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.CharacterControls.Disable();
+    }
+    void Update() 
+    {
+        if (_isInRange)
+        {
+            if (_interactPressed) 
+            {
+                _interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            _isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            _isInRange = false;
+        }
+    }
+}
