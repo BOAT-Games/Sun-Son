bf9f1e6af996d80fe3cbfb22b291ab36a9525df8
Austin Shin <amshin@calpoly.edu>
2021-03-01 22:25:31 -0800

    Create progression barrier and collectibles

---
+++ Sun Son/Assets/Scripts/ProgressionBarrier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionBarrier : MonoBehaviour
+{
+   private GameObject barrierGO;
+   private ProgressionBarrier barrier;
+
+   [SerializeField] GameObject[] collectibles;
+   public int totalCollectibles;
+   public int collectiblesRemaining;
+
+   private void Start() {
+      totalCollectibles = collectibles.Length;
+      collectiblesRemaining = totalCollectibles;
+      barrierGO = GameObject.FindGameObjectWithTag("Barrier");
+      barrier = barrierGO.GetComponent<ProgressionBarrier>();
+   }
+
+   private void OnTriggerEnter(Collider other) {
+      if (other.tag == "Player" && collectiblesRemaining == 0) {
+         Destroy(barrierGO);
+         Destroy(gameObject);
+      }
+   }
+
+   public void DecrementRemaining() {
+      collectiblesRemaining -= 1;
+   }
+}
