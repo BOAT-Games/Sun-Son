e06360be747d7b15ac72d7887fbec9d6292cd63c
B-Iglesia <bradiglesia@gmail.com>
2021-03-03 19:14:33 -0800

    WIP: More work added. Creating commit to checkout a PR

--- Sun Son/Assets/MainMenuSoundController.cs
+++ Sun Son/Assets/MainMenuSoundController.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MainMenuSoundController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject _play;
+    private GameObject _options;
+    private GameObject _quit;
     void Start()
     {
-        
+        _play = GameObject.Find("/Canvas/MainMenu/PlayButton");
+        _options = GameObject.Find("/Canvas/MainMenu/OptionsButton");
+        _quit = GameObject.Find("/Canvas/MainMenu/QuitButton");
     }
 
     // Update is called once per frame
