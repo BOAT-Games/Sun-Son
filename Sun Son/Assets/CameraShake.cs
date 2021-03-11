2b1ef530026acd6176aab4aca75cd75a059230c1
oerlich <oren.e@live.com>
2021-03-11 00:43:06 -0800

    Caves Boss Cutscene

---
+++ Sun Son/Assets/CameraShake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0f;
+
+    }
+
+    public void EnableShake()
+    {
+        GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 2.5f;
+    }
+}
--- Sun Son/Assets/Scripts/SceneController.cs
+++ Sun Son/Assets/Scripts/SceneController.cs
@@ -8,6 +8,8 @@ public class SceneController : MonoBehaviour
     [SerializeField] string SceneToLoad;
     [SerializeField] SceneFader _fader;
 
+    private bool initiated = false;
+
    private void Start() {
       if (SceneToLoad.Equals("Tutorial_Level")) {
          PlayerPrefs.DeleteAll();
@@ -16,8 +18,27 @@ public class SceneController : MonoBehaviour
 
    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !initiated)
         {
+            initiated = true;
+            if (SceneToLoad.Equals("Village2"))
+            {
+                PlayerPrefs.SetInt("dash", 1);
+                _fader.specialFade();
+            }
+            else
+            {
+                _fader.normalFade();
+            }
+            _fader.FadeToLevel(SceneToLoad);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !initiated)
+        {
+            initiated = true;
             if (SceneToLoad.Equals("Village2"))
             {
                 PlayerPrefs.SetInt("dash", 1);
