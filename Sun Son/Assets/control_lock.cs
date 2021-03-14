796bb59632ad79e2c64c411b83f26c4d75b725e5
Damien Trunkey <dtrunkey@calpoly.edu>
2021-03-14 15:21:28 -0700

    intro cutscene revamp
    Made a whole new cutscene, added the scene to the build settings, and added a lockControls prefab to drag into a cutscene and lock suns movement.

---
+++ Sun Son/Assets/control_lock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control_lock : MonoBehaviour
+{
+    public GameObject other;
+    // Start is called before the first frame update
+    void Start()
+    {
+        other.GetComponent<PlayerV2>().lockControls(99999999);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
